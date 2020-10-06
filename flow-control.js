fc14c9695501d58cfba09fe15c289f6a6bee9de3
Petar Cale <usavatreni@gmail.com>
2020-10-06 00:40:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,9 +15,9 @@ if(age = 13 && age < 20) {
 function ageChecker(age) {
 if(age <= 12) {
   console.log("You are a kid")
-} else if( age = 13 && age < 20) {
+} else if (age > 12 && age < 20) {
   console.log("You are a teenager!")
-} else {
+} else (age > 19) {
   console.log("You are a grownup")
 }
 }
