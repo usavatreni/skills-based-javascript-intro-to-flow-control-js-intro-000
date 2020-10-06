50e29ded346582c1fce86572ca4f1a5fb0745cf0
Petar Cale <usavatreni@gmail.com>
2020-10-06 01:28:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,13 +28,5 @@ return (age >=13 && age <=19 ? 'You are a teenager' : "You are not a teenager");
 }
 
 function switchAge(age) {
-switch (age) {
-  case age >=13 && age <=19:
-  return "You are a teenager";
-   break;
-   default:
-     return "You have an age"
-     break;
 
-}
 }
