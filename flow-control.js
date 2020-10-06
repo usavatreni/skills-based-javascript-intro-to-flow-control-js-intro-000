f41602638a2ad894da0ecdc350e7b23494c9802d
Petar Cale <usavatreni@gmail.com>
2020-10-06 00:35:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,25 @@
 function basicTeenager(age) {
-
+if(age = 13 && age < 20) {
+  return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+if(age = 13 && age < 20) {
+  return "You are a teenager!"
+} else {
+  return "You are not a teenager";
+}
 }
 
 function ageChecker(age) {
-
+if(age <= 12) {
+  console.log("You are a kid")
+} else if( age = 13 && age < 20) {
+  console.log("You are a teenager!")
+} else {
+  console.log("You are a grownup")
+}
 }
 
 function ternaryTeenager(age) {
